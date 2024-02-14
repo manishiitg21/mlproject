38644e0b7f68d01300ca278aaf565cd0b07e0c3f
mknemua <mknemua123@gmail.com>
2024-02-14 11:28:32 +0530

    data transformation.ipynb

---
+++ src/components/data_transformation.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from sklearn.dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumTransformer
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
+    
+class DataTransformation:
+    def __init__:
+        
+        self.obj_location=DataTransfromationConfig
+        
+        
+    
+    
